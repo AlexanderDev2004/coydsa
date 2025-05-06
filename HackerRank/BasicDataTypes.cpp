8ec5ecf7bd1dd53daf84e32e38908b053da7b02e
AlexanderDev2004 <alexgaming2120@gmail.com>
2025-05-06 21:21:20 +0700

    feat(hackerrank) : basic Data Type in cpp

---
+++ HackerRank/BasicDataTypes.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int a = 3;
+    long w = 12345678912345;
+    char ch = 'a';
+    float c = 334.23;
+    double x = 14049.304930000;
+    
+    printf("%d\n", a);
+    printf("%ld\n", w);
+    printf("%c\n", ch);
+    printf("%.3f\n", c);
+    printf("%.9lf\n", x);
+
+    return 0;
+}
