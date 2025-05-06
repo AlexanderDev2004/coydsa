f0c7d5685e284e564f30ed1a982ab259a27dcade
AlexanderDev2004 <alexgaming2120@gmail.com>
2025-05-06 21:23:51 +0700

    refactor: clean up code by removing commented-out variables and reformatting input/output

--- HackerRank/BasicDataTypes.cpp
+++ HackerRank/BasicDataTypes.cpp
@@ -7,18 +7,31 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-    int a = 3;
-    long w = 12345678912345;
-    char ch = 'a';
-    float c = 334.23;
-    double x = 14049.304930000;
-    
-    printf("%d\n", a);
-    printf("%ld\n", w);
-    printf("%c\n", ch);
-    printf("%.3f\n", c);
-    printf("%.9lf\n", x);
 
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+
+    cin >> a >> b >> c >> d >> e;
+    cout << a << endl << b << endl << c << endl;
+    printf("%.3f\n", d);
+    printf("%.9f\n", e);    
     return 0;
+    
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    // int a = 3;
+    // long w = 12345678912345;
+    // char ch = 'a';
+    // float c = 334.23;
+    // double x = 14049.304930000;
+    
+    // printf("%d\n", a);
+    // printf("%ld\n", w);
+    // printf("%c\n", ch);
+    // printf("%.3f\n", c);
+    // printf("%.9lf\n", x);
+
+    // return 0;
 }
